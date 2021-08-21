f497d27246f853200f56a806d33f9f5149feee98
SOUBHAGYA32 <soubhagya801832@gmail.com>
2021-08-21 21:59:06 +0530

    Ejs Layout Set Up

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require('express')
 const ejs = require('ejs')
 const path = require('path')
+const expressLayout = require('express-ejs-layouts')
 const app=express()
 
 const PORT = process.env.PORT || 3000
 
 
-app.get('/' , (req , res)=>{
-
-   res.render('index')
-
-})
 
 // Assets
 app.use(express.static('public'))
 
 
-//Set Template Engine
-app.set("view engine", "ejs")
+
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+app.get('/' , (req , res)=>{
+
+    res.render('pages/home')
+ 
+ })
+app.get('/cart' , (req , res)=>{
+ 
+    res.render('customers/cart')
+ 
+ })
 
 app.listen(PORT , ()=>{
     console.log(`Listening on port ${PORT}`)
