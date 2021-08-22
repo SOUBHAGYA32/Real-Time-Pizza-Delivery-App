aec53a362afe1c7a26cdc91b7e6b33dab7bbb303
SOUBHAGYA32 <soubhagya801832@gmail.com>
2021-08-22 19:03:39 +0530

    Rotes and Controller Completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const app=express()
 
+
 const PORT = process.env.PORT || 3000
 
 
@@ -19,26 +20,13 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 
 
-app.get('/' , (req , res)=>{
+//Routes 
+require('./routes/router')(app)
 
-    res.render('pages/home')
- 
- })
-app.get('/cart' , (req , res)=>{
- 
-    res.render('customers/cart')
- 
- })
-app.get('/login' , (req , res)=>{
 
-    res.render('auth/login')
 
-})
-app.get('/signup' , (req , res)=>{
 
-    res.render('auth/signup')
 
-})
 
 app.listen(PORT , ()=>{
     console.log(`Listening on port ${PORT}`)
